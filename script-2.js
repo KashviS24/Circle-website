babcbec9128a9200fd3c135aae4781e8e7da3942
KashviS24 <sherk758@newschool.edu>
2024-10-15 15:47:25 -0400

    updated flip game and home button

--- script-2.js
+++ script-2.js
@@ -2,24 +2,58 @@ const gameContainer = document.querySelector('.game-container');
 
 // Only create the grid if we are on the flip game page
 if (gameContainer) {
-  // Define 10 unique colors (for 10 pairs, 20 cards)
+  // Define 10 unique image URLs (located in the "Assets" folder)
+  const images = [
+    'Assets/image1.jpg', 'Assets/image2.jpg', 'Assets/image3.jpg', 'Assets/image4.jpg',
+    'Assets/image5.jpg', 'Assets/image6.jpg', 'Assets/image7.jpg', 'Assets/image8.jpg',
+    'Assets/image9.jpg', 'Assets/image10.jpg'
+  ];
+
+  // Define 10 unique colors for plain cards
   const colors = [
     '#8A9A5B', '#FF5733', '#33FF57', '#96DED1', '#3357FF', '#8B0000',
     '#FF33A5', '#F88379', '#A533FF', '#D8BFD8'
   ];
 
-  // Shuffle the colors and create a deck of 10 pairs (20 cards)
-  let shuffledColors = shuffle([...colors, ...colors]);
+  // Pair each image with a corresponding color
+  let pairedCards = [];
+  for (let i = 0; i < images.length; i++) {
+    pairedCards.push({
+      type: 'image',
+      value: images[i],
+      pairColor: colors[i] // Store corresponding color for border
+    });
+    pairedCards.push({
+      type: 'color',
+      value: colors[i]
+    });
+  }
+
+  // Shuffle the pairs of cards (20 total)
+  let shuffledCards = shuffle(pairedCards);
 
   // Create a 4x5 grid with 20 cards
   function createGrid() {
     for (let i = 0; i < 20; i++) {
       const card = document.createElement('div');
       card.classList.add('card');
-      card.innerHTML = `
-        <div class="front"></div>
-        <div class="back" style="background-color: ${shuffledColors[i]}"></div>
-      `;
+
+      if (shuffledCards[i].type === 'image') {
+        // If it's an image, apply the image and set the border to match its pair
+        card.innerHTML = `
+          <div class="front"></div>
+          <div class="back" style="background-image: url(${shuffledCards[i].value}); 
+                                   background-size: cover; 
+                                   border: 5px solid ${shuffledCards[i].pairColor};"></div>
+        `;
+      } else {
+        // If it's a color card, apply the color
+        card.innerHTML = `
+          <div class="front"></div>
+          <div class="back" style="background-color: ${shuffledCards[i].value};"></div>
+        `;
+      }
+
       card.addEventListener('click', flipCard);
       gameContainer.appendChild(card);
     }
@@ -65,7 +99,14 @@ function flipCard() {
 
 // Check if the flipped cards match
 function checkForMatch() {
-  let isMatch = firstCard.querySelector('.back').style.backgroundColor === secondCard.querySelector('.back').style.backgroundColor;
+  let isMatch = false;
+
+  // If one card is an image, the other should be its corresponding color
+  if (firstCard.querySelector('.back').style.backgroundImage && secondCard.querySelector('.back').style.backgroundColor) {
+    isMatch = firstCard.querySelector('.back').style.borderColor === secondCard.querySelector('.back').style.backgroundColor;
+  } else if (firstCard.querySelector('.back').style.backgroundColor && secondCard.querySelector('.back').style.backgroundImage) {
+    isMatch = firstCard.querySelector('.back').style.backgroundColor === secondCard.querySelector('.back').style.borderColor;
+  }
 
   if (isMatch) {
     disableCards();
@@ -96,4 +137,5 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
   lockBoard = false;
 }
+
 
